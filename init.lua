16b4a5d442a66fc982ca09b9f894b401da838701
Jayadeep Karnati <karnatijayadeep@gmail.com>
2025-08-11 02:44:00 +0530

    Add Ctrl+Space keymap to open Claude in vertical split
    - Added keymap for <C-Space> to open terminal in vertical split
    - Automatically sources .zshrc, uses Node 22 via nvm, and runs claude with permissions
    - Provides quick access to Claude Code from within Neovim

--- init.lua
+++ init.lua
@@ -122,6 +122,9 @@ vim.keymap.set('t', '<C-l>', '<C-\\><C-n><C-w>l', { desc = 'Move focus to the ri
 -- Open terminal in vertical split and switch to insert mode
 vim.keymap.set('n', '<C-t>', '<cmd>vsplit | terminal<CR>i', { desc = 'Open terminal in vertical split and enter insert mode' })
 
+-- Open terminal in vertical split and run claude command
+vim.keymap.set('n', '<C-Space>', '<cmd>vsplit | terminal source ~/.zshrc && nvm use 22 && claude --dangerously-skip-permissions<CR>i', { desc = 'Open claude in vertical split' })
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
