3f6610a21879ee1cee824b1587d086afd99b9d06
Jayadeep Karnati <karnatijayadeep@gmail.com>
2025-09-18 17:14:49 +0530

    Add 'q' keymap to quit fugitive buffers
    Adds autocmd to map 'q' key to quit in fugitive and git buffers
    for easier navigation and buffer management.

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -10,5 +10,13 @@ return {
     map('n', '<leader>gb', '<cmd>Git blame<cr>', { desc = '[G]it [b]lame' })
     map('n', '<leader>gd', '<cmd>Gdiffsplit<cr>', { desc = '[G]it [d]iff' })
     map('n', '<leader>gc', '<cmd>Git commit<cr>', { desc = '[G]it [c]ommit' })
+    
+    -- Map 'q' to quit in fugitive buffers
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "fugitive*", "git" },
+      callback = function()
+        vim.keymap.set("n", "q", ":q<CR>", { buffer = true, silent = true })
+      end,
+    })
   end,
 }
