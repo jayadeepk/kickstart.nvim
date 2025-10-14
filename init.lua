6b6ae1e3d80dbd2f99f10aebd1da5e23cc3cd0d4
Jayadeep Karnati <karnatijayadeep@gmail.com>
2025-10-15 01:43:51 +0530

    Disable EditorConfig support in Neovim

--- init.lua
+++ init.lua
@@ -7,6 +7,9 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+-- Disable EditorConfig support
+vim.g.editorconfig = false
+
 -- [[ Setting options ]]
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
