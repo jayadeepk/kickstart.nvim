59f9b73e78ee760b3528601e654865548e818bef
Jayadeep Karnati <karnatijayadeep@gmail.com>
2025-10-15 02:16:31 +0530

    Remove Conform.nvim autoformatter plugin
    Remove stevearc/conform.nvim plugin configuration to align with linux branch setup. This removes the <leader>f manual formatting keymap and the automatic formatting capabilities.

--- init.lua
+++ init.lua
@@ -655,34 +655,6 @@ require('lazy').setup({
     end,
   },
 
-  { -- Autoformat
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
-    },
-    opts = {
-      notify_on_error = false,
-      format_on_save = nil,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
-      },
-    },
-  },
-
   { -- Autocompletion
     'saghen/blink.cmp',
     event = 'VimEnter',
