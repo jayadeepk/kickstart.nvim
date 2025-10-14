e3bb6b85f6101ab2edade6de4a3a7d1121e52ead
Jayadeep Karnati <karnatijayadeep@gmail.com>
2025-10-15 02:49:53 +0530

    Remove trailing whitespace in plugin configurations

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -4,13 +4,13 @@ return {
   config = function()
     -- Basic keymaps for fugitive
     local map = vim.keymap.set
-    
+
     map('n', '<leader>gs', '<cmd>Git<cr>', { desc = '[G]it [s]tatus' })
     map('n', '<leader>gl', '<cmd>Git log<cr>', { desc = '[G]it [l]og' })
     map('n', '<leader>gb', '<cmd>Git blame<cr>', { desc = '[G]it [b]lame' })
     map('n', '<leader>gd', '<cmd>Gdiffsplit<cr>', { desc = '[G]it [d]iff' })
     map('n', '<leader>gc', '<cmd>Git commit<cr>', { desc = '[G]it [c]ommit' })
-    
+
     -- Map 'q' to quit in fugitive buffers
     vim.api.nvim_create_autocmd("FileType", {
       pattern = { "fugitive*", "git" },
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -17,16 +17,16 @@ return {
     config = function()
       local connections_file = os.getenv("HOME") .. "/.local/share/dbee/connections.json"
       local sources = {}
-      
+
       -- Only add FileSource if connections.json exists
       if vim.fn.filereadable(connections_file) == 1 then
         table.insert(sources, require("dbee.sources").FileSource:new(connections_file))
       end
-      
+
       require("dbee").setup({
         sources = sources,
       })
-      
+
       -- Auto-execute queries on save for dbee scratchpads
       vim.api.nvim_create_autocmd("BufWritePost", {
         pattern = "*.sql",
@@ -37,7 +37,7 @@ return {
             -- Get the entire buffer content like run_file does
             local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
             local query = table.concat(lines, "\n")
-            
+
             -- Execute the query (equivalent to pressing BB in normal mode)
             if query and query ~= "" then
               require("dbee").execute(query)
